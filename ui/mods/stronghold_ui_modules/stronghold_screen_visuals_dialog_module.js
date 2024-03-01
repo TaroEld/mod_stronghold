133dfd2648a2d0dc835157af3149c0abd64ac1be
taroeld <tarob@live.de>
2024-03-01 09:18:10 +0100

    improve visuals module

--- ui/mods/stronghold_ui_modules/stronghold_screen_visuals_dialog_module.js
+++ ui/mods/stronghold_ui_modules/stronghold_screen_visuals_dialog_module.js
@@ -8,7 +8,13 @@ var StrongholdScreenVisualsModule = function(_parent)
     this.mCurrentBaseSprite = null;
     this.mBaseSprite = null;
     this.mBaseSpriteIndex = null;
-    this.mBaseSpriteImages = [];
+    this.mSpriteImages = {
+    	Tier1 : null,
+    	Tier2 : null,
+    	Tier3 : null,
+    	Mercenaries : null,
+    	Hamlet : null
+    };
 };
 
 StrongholdScreenVisualsModule.prototype = Object.create(StrongholdScreenModuleTemplate.prototype);
@@ -21,24 +27,26 @@ StrongholdScreenVisualsModule.prototype.createDIV = function (_parentDiv)
 {
 	StrongholdScreenModuleTemplate.prototype.createDIV.call(this, _parentDiv);
     var self = this;
+    var text = this.getModuleText();
     this.mContentContainer.addClass("visuals-module");
     
-    var baseSpriteRow = this.mContentContainer.appendRow("", "base-sprites-row gold-line-bottom");
-    this.mSpriteNameLabel = baseSpriteRow.find(".sub-title");
-    for (var i = 0; i < 3; i++) {
-        var changeSpriteContainer = $('<div class="base-sprite-container"/>');
-        baseSpriteRow.append(changeSpriteContainer)
-        var baseSpriteImage = $('<img class="base-sprite-image"/>');
-        changeSpriteContainer.append(baseSpriteImage);
-        this.mBaseSpriteImages.push(baseSpriteImage)
-    }
-    
-    
-    var otherSpriteRow = this.mContentContainer.appendRow("Other Sprites", "other-sprites-row gold-line-bottom")
-    this.mUnitSpriteImage = $('<img class="unit-sprite-image"/>');
-    otherSpriteRow.append(this.mUnitSpriteImage)
-    this.mHouseSpriteImage = $('<img class="house-sprite-image"/>');
-    otherSpriteRow.append(this.mHouseSpriteImage)
+    var spritesHeader = this.mContentContainer.appendRow("");
+    this.mSpriteNameLabel = spritesHeader.find(".sub-title");
+    this.mSpritesContainer = this.mContentContainer.appendRow(null, "sprites-container stronghold-generic-background");
+
+    MSU.iterateObject(this.mSpriteImages, function(_key, _value){
+    	var changeSpriteContainer = $('<div class="sprite-container"/>');
+    	self.mSpritesContainer.append(changeSpriteContainer)
+    	self.mSpriteImages[_key] = $('<img class="sprite-image"/>');
+    	changeSpriteContainer.append(self.mSpriteImages[_key]);
+    	Stronghold.getTextDiv(Stronghold.Text.General[_key])
+    		.addClass("stronghold-flex-center")
+    		.appendTo(changeSpriteContainer);
+    });
+    this.mSpriteImages.Mercenaries.css({
+    	"width" : "10rem",
+    	"height" : "auto"
+    })
 
 
     var footerRow = this.mContentContainer.appendRow(null, "footer-button-bar");
@@ -52,10 +60,10 @@ StrongholdScreenVisualsModule.prototype.createDIV = function (_parentDiv)
     {
         self.switchSpriteImage(1);
     }, "switch-image", 3);
-    this.mApplySpritesButton = footerRow.createTextButton("Apply Sprites", function()
+    this.mApplySpritesButton = footerRow.createTextButton(text.Button, function()
     {
         self.changeSprites();
-    }, "apply-sprites-button", 1)
+    }, "apply-sprites-button", 1);
 };
 
 StrongholdScreenVisualsModule.prototype.getSpriteIndex = function()
@@ -68,30 +76,10 @@ StrongholdScreenVisualsModule.prototype.switchSpriteImage = function( _idx )
 	var arrLen = Stronghold.Visuals.BaseSpriteTypes.length;
 	var newIdx = this.getSpriteIndex() + _idx;
 	if(newIdx == arrLen) newIdx = 0;
-	if(newIdx < 0) newIdx = arrLen - 1
+	if(newIdx < 0) newIdx = arrLen - 1;
 	this.mBaseSprite = Stronghold.Visuals.BaseSpriteTypes[newIdx];
     this.setSpriteImage();
 }
-
-StrongholdScreenVisualsModule.prototype.setSpriteImage = function()
-{
-	var currentArr = Stronghold.Visuals.Sprites[this.mBaseSprite];
-	var baseSize = this.mParent.mData.Size;
-
-    this.mBaseSpriteImages[0].attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.MainSprites[0] + ".png");
-    this.mBaseSpriteImages[1].attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.MainSprites[1] + ".png");
-    this.mBaseSpriteImages[2].attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.MainSprites[2] + ".png");
-
-	this.mUnitSpriteImage.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.UnitSprite + ".png");
-	this.mHouseSpriteImage.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.HouseSprites[0] + ".png");
-    var text = currentArr.Name + " by " + currentArr.Author
-    if(this.mCurrentBaseSprite == this.mBaseSprite)
-    {
-        text += (" (current visuals)")
-    }
-	this.mSpriteNameLabel.text(text)
-} 
-
 
 StrongholdScreenVisualsModule.prototype.loadFromData = function(_data)
 {
@@ -101,6 +89,21 @@ StrongholdScreenVisualsModule.prototype.loadFromData = function(_data)
     this.mCurrentBaseSprite = this.mData.TownAssets.SpriteName;
 	this.mBaseSpriteIndex = this.getSpriteIndex();
 	this.setSpriteImage()
+}
+
+StrongholdScreenVisualsModule.prototype.setSpriteImage = function()
+{
+	var currentArr = Stronghold.Visuals.Sprites[this.mBaseSprite];
+	var baseSize = this.mParent.mData.Size;
+	var text = this.getModuleText();
+
+    this.mSpriteImages.Tier1.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.MainSprites[0] + ".png");
+    this.mSpriteImages.Tier2.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.MainSprites[1] + ".png");
+    this.mSpriteImages.Tier3.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.MainSprites[2] + ".png");
+	this.mSpriteImages.Mercenaries.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.UnitSprite + ".png");
+	this.mSpriteImages.Hamlet.attr('src', Path.GFX + Stronghold.Visuals.SpritePath + currentArr.HouseSprites[0] + ".png");
+	var name = Stronghold.Text.format(text.SpriteName, currentArr.Name, currentArr.Author, this.mCurrentBaseSprite == this.mBaseSprite ? text.Current : "")
+	this.mSpriteNameLabel.text(name)
 }
 
 StrongholdScreenVisualsModule.prototype.changeSprites = function ()
